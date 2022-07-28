7e2e6d9d57d7ebfc4b88d61105cf2a8bb10b8b1f
Gino Filicetti <ginof@google.com>
2022-07-28 16:40:41 -0400

    removed timing from the commands because it is not cross platform friendly

--- argolis-enable-apis.sh
+++ argolis-enable-apis.sh
@@ -164,7 +164,10 @@ do
 
     # if on a mac, you need GNU time (`gtime`) installed with: brew install gtime
     # if on regular linux, just use regular `time` instead.
-    (gtime -f "%e" gcloud services enable $api --project $project) 2>&1 | xargs printf "Finished in %.0fs\n"
+    #(gtime -f "%e" gcloud services enable $api --project $project) 2>&1 | xargs printf "Finished in %.0fs\n"
+
+    # just run the command without timing as the above is not cross platform compatible
+    gcloud services enable $api --project $project
 
 done
 
